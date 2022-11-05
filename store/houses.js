ecebb459bf35bfb08a0d921ba25277818c47d394
Bartlomiej95 <panfilbartlomiej@gmail.com>
2022-11-05 17:03:44 +0100

    Create store for houses

---
+++ store/houses.js
@@ -0,0 +1,5 @@
+import {houseForAnimals} from "@/data/data";
+
+export const state = () => ({
+  items: [...houseForAnimals],
+});
---
+++ store/index.js
@@ -0,0 +1,7 @@
+export const state = () => ({
+  houses: []
+})
+
+export const actions = {
+
+}
