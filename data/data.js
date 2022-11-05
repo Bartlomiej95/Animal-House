a5cee79173e121738ec4961ab7946c0e23ebc339
Bartlomiej95 <panfilbartlomiej@gmail.com>
2022-11-05 14:31:37 +0100

    Added data with example houses

---
+++ data/data.js
@@ -0,0 +1,39 @@
+import {Category} from'./house-category';
+
+export const houseForAnimals = [
+  {
+    id: 'd8520eb3-f57e-4faf-a0fa-238052e71cc6',
+    name: "Domek mały",
+    category: Category.ONTREE,
+    price: 100,
+    currency: 'PLN',
+  },
+  {
+    id: 'abf99690-f065-41d1-b344-30d55b5ef679',
+    name: "Karmnik",
+    category: Category.ONTREE,
+    price: 50,
+    currency: 'PLN',
+  },
+  {
+    id: 'dca18bd9-eff9-465d-8556-09c4361ec67f',
+    name: "Zwierzęcy apartament",
+    category: Category.ONTHEGROUND,
+    price: 300,
+    currency: 'PLN',
+  },
+  {
+    id: '9dba2eb2-a67c-4cbc-97d2-91a6232b7c13',
+    name: "Domek średni",
+    category: Category.ONTHEGROUND,
+    price: 200,
+    currency: 'PLN',
+  },
+  {
+    id: '56da6845-7398-444b-a6d1-1aaaa712f96c',
+    name: "Zwierzęca willa",
+    category: Category.HOUSEPROJECT,
+    price: 500,
+    currency: 'PLN',
+  },
+]
---
+++ data/house-category.js
@@ -0,0 +1,5 @@
+export const Category = {
+  ONTREE: 'Domki na drzewie',
+  ONTHEGROUND: 'Domki na ziemi',
+  HOUSEPROJECT: 'Domki na zamówienie'
+}
