07738af3cf9db20912c6a3b2d10a16239f9b2d08
Bartlomiej95 <panfilbartlomiej@gmail.com>
2022-11-06 21:39:25 +0100

    Added data with different exchange rates

---
+++ data/currency.js
@@ -0,0 +1,31 @@
+export const currencyData = [
+  {
+    currency: 'PLN',
+    symbol: 'PLN',
+  },
+  {
+    currency: 'USD',
+    symbol: '$',
+  },
+  {
+    currency: 'EUR',
+    symbol: '€',
+  },
+  {
+    currency: 'CHF',
+    symbol: 'CHF',
+  },
+  {
+    currency: 'CNY',
+    symbol: 'CNY',
+  },
+  {
+    currency: 'GBP',
+    symbol: '£',
+  },
+  {
+    currency: 'JPY',
+    symbol: '¥',
+  },
+
+]
