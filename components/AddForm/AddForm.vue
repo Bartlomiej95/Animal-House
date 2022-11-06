bfa976f64a332154c44e4f87c5455e5a6a67ba84
Bartlomiej95 <panfilbartlomiej@gmail.com>
2022-11-06 21:00:14 +0100

    Added validation to form with adding new product

--- components/AddForm/AddForm.vue
+++ components/AddForm/AddForm.vue
@@ -13,6 +13,9 @@
       <label>Cena</label><br/>
       <input type="number" name="price" placeholder="Cena produktu" v-model="newItem.price" /><br/>
       <button class="form-add-btn" type="submit">Dodaj</button>
+      <div v-if="this.errorBoolean" class="wrapper-error">
+        <p>{{errorMsg}}</p>
+      </div>
     </form>
   </div>
 </template>
@@ -25,15 +28,45 @@ export default {
       newItem: {
         name: '',
         category: '',
-        price: 0
+        price: 0,
       },
       category: Category,
+      errorMsg: '',
+      errorBoolean: false,
     }
   },
   methods: {
     addNew(house){
+      console.log(house);
+      this.validate(house);
+      if(this.errorBoolean){
+        return
+      }
+      console.log('za validate')
       this.$store.commit('houses/add', house);
       this.$router.push('/')
+    },
+
+    validate(data){
+      this.errorBoolean = false;
+      console.log(data.category === Category.ONTREE)
+      if(!data.name || !data.category || !data.category){
+        this.errorBoolean = true;
+        this.errorMsg = 'Please fill every field';
+      }
+      if(data.name.length < 3 || data.name.length > 50){
+        this.errorBoolean = true;
+        this.errorMsg = 'Name should has more than 3 characters and less than 50 characters';
+      }
+      if(data.price <= 0){
+        this.errorBoolean = true;
+        this.errorMsg = 'Price has to be a positive value';
+      }
+      if(!(data.category === Category.ONTREE) && !(data.category === Category.ONTHEGROUND) && !(data.category === Category.HOUSEPROJECT)){
+        this.errorBoolean = true;
+        this.errorMsg = `Price choose one from available option:${Category.ONTREE}, ${Category.ONTHEGROUND},
+          ${Category.HOUSEPROJECT}`;
+      }
     }
   }
 }
@@ -88,4 +121,9 @@ export default {
     background-color: #485a6f;
     /*color: #4dbe7f;*/
   }
+
+  .wrapper-error p{
+    color: red;
+  }
+
 </style>
